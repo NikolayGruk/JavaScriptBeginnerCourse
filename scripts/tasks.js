6d0545d230f6088f6f4d32c81c2b72c060c10311
Николай Грук < >
2016-12-08 01:10:24 +0300

    done task 1 and 2

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,5 +1,5 @@
-// 1) Написать функцию getFieldValues, которая будет принимать на вход массив объектов, 
-// а возвращать будет массив значений одного из полей (отсортированных в порядке возрастания):
+/* 1) Написать функцию getFieldValues, которая будет принимать на вход массив объектов, 
+ а возвращать будет массив значений одного из полей (отсортированных в порядке возрастания):
 
 var usersData = [
 	{ 'user' : 'Alex', 'password' : 'MyNameIsAlex' },
@@ -7,11 +7,57 @@ var usersData = [
 ];
 console.log(getFieldValues(usersData, 'user')); // --> ['Alex', 'Bob']
 
-// 2) Написать функцию, фильтрующую массив с использованием предиката:
+*/
+
+var usersData = [ 
+{ 'user' : 'Alex', 'password' : 'MyNameIsAlex' }, 
+{ 'user' : 'Bob', 'password' : 'MyNAmeIsBob' } 
+]; 
+var scope = "global"; 
+function scopeTest() { 
+console.log(scope); // "undefined" 
+var scope = "local"; 
+console.log(scope); // "local" 
+} 
+console.log(getFieldValues(usersData, 'user')); // —> ['Alex', 'Bob'] 
+function getFieldValues(usersData, value){ 
+var output = []; 
+for(let i = 0; i < usersData.length; i++){ 
+output[i] = usersData[i][value]; 
+} 
+for (let i = 0; i < (output.length - 1); i++){ 
+for (let j = 0; j < (output.length -1 ); j++){ 
+if(output[j] > output[j + 1]){ 
+let c = output[j]; 
+output[j] = output[j + 1]; 
+output[j + 1] = c; 
+} 
+} 
+} 
+return output; 
+} 
+
+/* 2) Написать функцию, фильтрующую массив с использованием предиката:
 
 var numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
 function isEven(x) {/* Проверка на чётность */}
 console.log(filter(numbers, isEven)); // --> [2, 8, 34]
+*/
+var numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55]; 
+console.log(isEven(numbers)); 
+function isEven(x) { 
+var a = [], k = 0; 
+
+for(let i = 0; i < x.length; i++){ 
+if (x[i]%2 == 0){ 
+a[k] = x[i]; 
+k++; 
+} 
+} 
+return a; 
+}
+
+
 
 // 3) Даны 2 строки со словами (без знаков препинания), 
 // вывести те слова (по одному разу), которые встречаются в обоих строках
